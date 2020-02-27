d9a89b76b2b121e92bcf7eaa5e31e35c001a9488
Mahabubur Rahman Kausar <kausar@eatlbd.com>
2020-02-27 17:39:05 +0600

    ehance loop

--- src/info/kausar/Array.java
+++ src/info/kausar/Array.java
@@ -10,25 +10,26 @@ public class Array {
         employee[2] = 12;
         employee[3] = 14;
 
-        employee[0] = 100; //again set value and it will be the result
+        employee[0] = 100; //set new value
 
-        //System.out.println(employee[0]); //for particular index
+        //System.out.println(employee[0]); //for printing particular index
         for (int i = 0; i < 4; i++) {
             System.out.println(employee[i]);
         }
 
         int d [] [] = {
-                {1,2,3},
-                {4,5,6},
+                {1},
+                {4,5},
                 {7,8,9},
-                {10,11,12},
+                {1,3,5,7}, //J  agged Array .
+                {1,3,5,7,9}, // if number of index is not assigned then it calls jagges array
         };
-        System.out.println("First row third element = " + d[0][2]); //to print specific element
+        System.out.println("First row third element = " + d[0][0]); //to print specific element
 
         //for print array in array / 2d array
 
-        for (int i =0; i<4; i++){ //for number of array
-            for (int j =0; j<3; j++ ){ // for number of elements
+        for (int i =0; i<d.length; i++){ //for number of array
+            for (int j =0; j<d[i].length; j++ ){ // for number of elements
                 System.out.print(d[i][j]);
             }
             System.out.println();
