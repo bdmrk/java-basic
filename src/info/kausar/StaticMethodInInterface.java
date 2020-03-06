30df39b3fa6e6a58e7ff4595c1d8ad27c94b9b02
Mahabubur Rahman Kausar <hostkausar@gmail.com>
2020-03-06 19:17:54 +0600

    interface static method and variable

---
+++ src/info/kausar/StaticMethodInInterface.java
@@ -0,0 +1,30 @@
+package info.kausar;
+
+//Java 1.8 there is static method in interface
+//By default if variable inside the interface became constant
+//abstract method method, default method and static method
+
+interface StaticAbc {
+    int num = 100;
+
+    void abc();
+
+    static void show() {
+        System.out.println("Hello this is Static method of  StaticAbc");
+    }
+}
+
+class VariableIml implements StaticAbc {
+
+    public void abc() {
+//        num = 10;
+    }
+}
+
+
+public class StaticMethodInInterface {
+
+    public static void main(String[] args) {
+        StaticAbc.show();
+    }
+}
