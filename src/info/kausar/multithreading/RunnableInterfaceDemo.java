f0417222d47be6dfa4885125b655f57c0bdc48ae
Mahabubur Rahman Kausar <kausar@eatlbd.com>
2020-03-12 09:49:15 +0600

    Runnable Interface in multi Theading

---
+++ src/info/kausar/multithreading/RunnableInterfaceDemo.java
@@ -0,0 +1,41 @@
+package info.kausar.multithreading;
+
+class Hi2 implements Runnable {
+
+    @Override
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("hi");
+            try {
+                Thread.sleep(1000);
+            }catch (Exception e){}
+        }
+    }
+}
+
+class Hello2 implements Runnable {
+
+    @Override
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("hello");
+            try {
+                Thread.sleep(1000);
+            }catch (Exception e){}
+        }
+    }
+}
+
+public class RunnableInterfaceDemo {
+    public static void main(String[] args) {
+        Runnable obj = new Hi2();
+        Runnable obj2 = new Hello2();
+
+        Thread t1 = new Thread(obj);
+        Thread t2 = new Thread(obj2);
+        t1.start();
+        try { Thread.sleep(10); } catch (Exception e) { }
+        t2.start();
+
+    }
+}
