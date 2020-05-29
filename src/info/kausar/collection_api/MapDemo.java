76cbfc74f866df3594ec529018b88d877ccdc928
Mahabubur Rahman Kausar <hostkausar@gmail.com>
2020-05-29 11:10:30 +0600

    updated

--- src/info/kausar/collection_api/MapDemo.java
+++ src/info/kausar/collection_api/MapDemo.java
@@ -13,7 +13,7 @@ public class MapDemo {
         map.put("myColleague", "Shafi");
         // HashMap and HashSet both use HashCoding(Fetch the nearest value while saving)
         System.out.println(map);
-
+ 
 
         //to fetch value with the help of key we need to set value with Set
         Set<String> keys = map.keySet();
