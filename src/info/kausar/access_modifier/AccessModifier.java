ff8603e4712f77391b753a0557b0a7fcd546e822
kausar <kausar@eatlbd.com>
2024-06-15 09:16:23 +0600

    multiple implements

--- src/info/kausar/access_modifier/AccessModifier.java
+++ src/info/kausar/access_modifier/AccessModifier.java
@@ -1,6 +1,11 @@
 package info.kausar.access_modifier;
 
 import com.kausar.modifier.Student;
+// Public
+// private
+// protected
+// default
+// abstract
 
 public class AccessModifier extends Student {
    public static void main(String[] args) {
